6987c4a0d7b7903cf62f153b4b2584131ce06f6f
Jeroen Bomert <bomert@gmail.com>
2019-09-29 16:55:35 +0200

    working on config

--- config/config.go
+++ config/config.go
@@ -1,10 +1,35 @@
 package config
 
-var Config struct {
+import (
+	"sync"
+
+	"github.com/JeremyLoy/config"
+)
+
+type Config struct {
 	UseSunspec bool   `config:"USE_SUNSPEC"`
 	SunspecIp  string `config:"SUNSPEC_IP"`
 	UseYouless bool   `config:"USE_YOULESS"`
 	YoulessIp  string `config:"YOULESS_IP"`
 	UseSolcast bool   `config:"USE_SOLCAST"`
 	SolcastUrl string `config:"SOLCAST_URL"`
+}
+
+var cfg Config
+var mux sync.RWMutex
+
+func ReloadConfig() {
+	mux.Lock()
+	config.FromEnv().To(&cfg)
+	mux.Unlock()
+}
+
+func init() {
+	ReloadConfig()
+}
+
+func GetConfig() Config {
+	mux.RLock()
+	defer mux.RUnlock()
+	return cfg
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/recadra/data-collector
 go 1.13
 
 require (
+	github.com/JeremyLoy/config v1.1.0
 	github.com/goburrow/modbus v0.1.0
 	github.com/goburrow/serial v0.1.0 // indirect
 )
