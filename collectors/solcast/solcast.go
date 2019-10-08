ec629b0c41aef2a3d6f54799126033f20de9e31c
Jeroen Bomert <bomert@gmail.com>
2019-10-08 14:53:31 +0200

    run solcast async

--- collectors/solcast/solcast.go
+++ collectors/solcast/solcast.go
@@ -9,54 +9,69 @@ import (
 
 //Solcast is a connection to the Solcast API
 type Solcast struct {
-	addr   string
-	apiKey string
-	c      uint64
+	addr    string
+	apiKey  string
+	c       uint64
+	cl      *http.Client
+	runChan chan *storage.Influx
 }
 
 //NewSolcast creates a new Solcast to connect to the Solcast API
 func NewSolcast(addr, apiKey string) *Solcast {
-	return &Solcast{
-		addr:   addr,
-		apiKey: apiKey,
-		c:      3 * 60,
+	solcast := &Solcast{
+		addr:    addr,
+		apiKey:  apiKey,
+		c:       3 * 60 * 60,
+		cl:      &http.Client{},
+		runChan: make(chan *storage.Influx, 30),
 	}
+	go solcast.run()
+	return solcast
 }
 
 //Run gets the information from the Solcast API, parses it and
 //submits it to InfluxDB
 func (s *Solcast) Run(influx *storage.Influx) {
-	if s.c++; s.c < (3 * 60) {
-		//We run every 3 hours. Run is called every second by the
-		//main loop so count until 3 hours passed.
-		return
-	}
+	s.runChan <- influx
+}
 
-	req, err := http.NewRequest("GET", s.addr, nil)
-	if err != nil {
-		log.Printf("Solcast Request Error: %s\n", err.Error())
-		return
-	}
-	req.Header.Add("Authorization", "Bearer "+s.apiKey)
+//run is a parallel runner thread for the Solcast data, because Solcast data
+//takes some time to receive and parse
+func (s *Solcast) run() {
+	for {
+		influx := <-s.runChan
+		if s.c++; s.c < (3 * 60 * 60) {
+			//We run every 3 hours. Run is called every second by the
+			//main loop so count until 3 hours passed.
+			continue
+		}
 
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		log.Printf("Solcast Get Error: %s\n", err.Error())
-		return
+		req, err := http.NewRequest("GET", s.addr, nil)
+		if err != nil {
+			log.Printf("Solcast Request Error: %s\n", err.Error())
+			continue
+		}
+		req.Header.Add("Authorization", "Bearer "+s.apiKey)
+
+		resp, err := s.cl.Do(req)
+		if err != nil {
+			log.Printf("Solcast Get Error: %s\n", err.Error())
+			continue
+		}
+		data, err := NewData(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			log.Printf("Solcast NewData Error: %v\n", err.Error())
+			log.Printf("Got status: %v\n", resp.Status)
+			continue
+		}
+		err = influx.Send(data.ToInflux())
+		if err != nil {
+			log.Printf("Solcast ToInflux Error: %v\n", err.Error())
+			continue
+		}
+		//No error occurred, reset counter
+		s.c = 0
+		log.Printf("Received Solcast data\n")
 	}
-	data, err := NewData(resp.Body)
-	resp.Body.Close()
-	if err != nil {
-		log.Printf("Solcast NewData Error: %v\n", err.Error())
-		log.Printf("Got status: %v\n", resp.Status)
-		return
-	}
-	err = influx.Send(data.ToInflux())
-	if err != nil {
-		log.Printf("Solcast ToInflux Error: %v\n", err.Error())
-		return
-	}
-	//No error occurred, reset counter
-	s.c = 0
-	log.Printf("Received Solcast data\n")
 }
