a22e0364411881d2be4d75d6515ef590f4064dce
Jeroen Bomert <bomert@gmail.com>
2019-10-08 10:25:52 +0200

    Making sure solcast runs every 3 hours instead of every second

--- collectors/solcast/solcast.go
+++ collectors/solcast/solcast.go
@@ -10,16 +10,23 @@ import (
 //Solcast is a connection to the Solcast API
 type Solcast struct {
 	addr string
+	c    uint64
 }
 
 //NewSolcast creates a new Solcast to connect to the Solcast API
 func NewSolcast(addr string) *Solcast {
-	return &Solcast{addr}
+	return &Solcast{addr, 0}
 }
 
 //Run gets the information from the Solcast API, parses it and
 //submits it to InfluxDB
 func (s *Solcast) Run(influx *storage.Influx) {
+	if s.c++; s.c < (3 * 60) {
+		//We run every 3 hours. Run is called every second by the
+		//main loop so count until 3 hours passed.
+		return
+	}
+
 	resp, err := http.Get(s.addr)
 	if err != nil {
 		log.Printf("Error: %s\n", err.Error())
@@ -34,5 +41,8 @@ func (s *Solcast) Run(influx *storage.Influx) {
 	err = influx.Send(data.ToInflux())
 	if err != nil {
 		log.Printf("Error: %v\n", err.Error())
+		return
 	}
+	//No error occurred, reset counter
+	s.c = 0
 }
